6e9a62d3534a54f4cd7896150321cd34a644348a
Pranav Balu <51266456+pranavbalu1@users.noreply.github.com>
2024-04-29 16:30:10 -0500

    refactor views to fetch name from person table

--- views.sql
+++ views.sql
@@ -1,11 +1,9 @@
-
 -- VIEW 1 : VIP PATIENT VIEW
-
 DROP VIEW IF EXISTS VIP_Patient_View;
 CREATE VIEW VIP_Patient_View AS
 SELECT 
-    P.FName AS First_Name,
-    P.SName AS Last_Name,
+    PN.FName AS First_Name,
+    PN.SName AS Last_Name,
     P.Patient_id,
     MIN(A.Date) AS Enrollment_Date
 FROM 
@@ -24,29 +22,32 @@ JOIN
     APPOINTMENT A ON VIP.Patient_id = A.Patient_id
 JOIN 
     PATIENT P ON VIP.Patient_id = P.Patient_id
+JOIN 
+    PERSON PN ON P.Person_id = PN.Person_id
 GROUP BY 
-    VIP.Patient_id;
+    P.Patient_id;
 
 -- VIEW 2 : Preferred Healthcare Professional View
-
 DROP VIEW IF EXISTS Preferred_Healthcare_Professional_View;
 CREATE VIEW Preferred_Healthcare_Professional_View AS
 SELECT 
     HP.Employee_id,
-    HP.FName AS First_Name,
-    HP.SName AS Last_Name,
+    PN.FName AS First_Name,
+    PN.SName AS Last_Name,
     COUNT(AP.Appointment_id) AS Patient_Interaction_Count
 FROM 
     HEALTHCARE_PROFESSIONAL HP
 JOIN 
     APPOINTMENT AP ON HP.Employee_id = AP.Employee_id
+JOIN 
+    PERSON PN ON HP.Person_id = PN.Person_id
 WHERE 
     AP.Date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
 GROUP BY 
     HP.Employee_id
 ORDER BY 
     Patient_Interaction_Count DESC;
-
+    
 -- View 3 : Critical Treatment View
 DROP VIEW IF EXISTS Critical_Treatment_View;
 CREATE VIEW Critical_Treatment_View AS
@@ -74,18 +75,19 @@ GROUP BY
 HAVING 
     Duplicate_Count > 5;
 
-
 -- View 4 
 DROP VIEW IF EXISTS Potential_VIP_Patient_View ;
 CREATE VIEW Potential_VIP_Patient_View AS
 SELECT
     P.Patient_id,
-    CONCAT(P.FName, ' ', COALESCE(P.MName, ''), ' ', P.SName) AS Full_Name,
+    CONCAT(PN.FName, ' ', COALESCE(PN.MName, ''), ' ', PN.SName) AS Full_Name,
     COUNT(A.Appointment_id) AS Appointment_Count
 FROM
     PATIENT P
 JOIN
     APPOINTMENT A ON P.Patient_id = A.Patient_id
+JOIN 
+    PERSON PN ON P.Person_id = PN.Person_id
 WHERE
     A.Date >= DATE_SUB(CURRENT_DATE(), INTERVAL 2 MONTH)
 GROUP BY
@@ -93,31 +95,35 @@ GROUP BY
     Full_Name
 HAVING
     Appointment_Count > 4;
-    
+
 -- vIEW 5
 DROP VIEW IF EXISTS Top_Staff_View ;
 CREATE VIEW Top_Staff_View AS
 SELECT
-    CONCAT(HP.FName, ' (', HP.Employee_id, ')') AS Staff_Name,
+    CONCAT(PN.FName, ' (', HP.Employee_id, ')') AS Staff_Name,
     'Healthcare Professional' AS Staff_Type,
     COUNT(*) AS Interaction_Count
 FROM
     HEALTHCARE_PROFESSIONAL HP
 JOIN
     APPOINTMENT A ON HP.Employee_id = A.Employee_id
+JOIN 
+    PERSON PN ON HP.Person_id = PN.Person_id
 WHERE
     A.Date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
 GROUP BY
     HP.Employee_id
 UNION
 SELECT
-    CONCAT(HP.FName, ' (', HP.Employee_id, ')') AS Staff_Name,
+    CONCAT(PN.FName, ' (', HP.Employee_id, ')') AS Staff_Name,
     'Healthcare Professional' AS Staff_Type,
     COUNT(*) AS Interaction_Count
 FROM
     HEALTHCARE_PROFESSIONAL HP
 JOIN
     TREATMENT T ON HP.Employee_id = T.Employee_id_prescriber OR HP.Employee_id = T.Employee_id_conductor
+JOIN 
+    PERSON PN ON HP.Person_id = PN.Person_id
 WHERE
     T.Start_date >= DATE_SUB(CURRENT_DATE(), INTERVAL 1 MONTH)
 GROUP BY
